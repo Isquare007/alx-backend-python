7698ecd010688cb631e026d28a5dad51dd522593
Isquare007 <akibrahiim2019@gmail.com>
2023-03-30 14:14:00 +0100

    .

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """type annotated function that floors a float"""
-from math import floor as Round
+from math import floor as roundup
 
 
 def floor(n: float) -> int:
     """floors a float"""
-    return round(n)
+    return roundup(n)
