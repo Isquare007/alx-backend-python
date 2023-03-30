38d44851b5f7d4d6a5a218dbb7d49c468be243b3
Isquare007 <akibrahiim2019@gmail.com>
2023-03-30 22:27:30 +0100

    5sum up

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,6 +2,7 @@
 """type-annotated function sum_list which takes a list input_list"""
 from typing import List
 
+
 def sum_list(input_list: List[float]) -> float:
     """sum up the float value of a list"""
     sumup = 0
