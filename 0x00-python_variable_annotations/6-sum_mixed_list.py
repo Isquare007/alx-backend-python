1bd1e5e8d8c10d2b9e76262e2514d29e5aa8faaa
Isquare007 <akibrahiim2019@gmail.com>
2023-03-30 23:05:08 +0100

    7 getting kv tupled

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -2,6 +2,7 @@
 """type annotion for list of mixed types"""
 from typing import List, Union
 
+
 def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
     """return the sum of a list of int and floats"""
 
