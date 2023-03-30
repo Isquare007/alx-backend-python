11f3598bcdff854a7ab789a3677a924bc3df6b73
Isquare007 <akibrahiim2019@gmail.com>
2023-03-30 13:54:34 +0100

    1 concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""concat annotations"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concatnates two strings and return the concated string"""
+    return str1 + str2
