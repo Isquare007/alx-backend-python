a638e18295f76371d46e1e4b238a811505cecea7
Isquare007 <akibrahiim2019@gmail.com>
2023-04-04 21:00:00 +0100

    py for 1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """async_generator from the previous task and then write a coroutine
 called async_comprehension that takes no arguments."""
-from typing import Generator
 async_generator = __import__('0-async_generator').async_generator
 
 
