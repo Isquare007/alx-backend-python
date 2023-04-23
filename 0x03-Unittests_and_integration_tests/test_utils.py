d44e10e91ec539fe6a81ed58453458cee2c53319
Isquare007 <akibrahiim2019@gmail.com>
2023-04-23 16:10:30 +0100

    task 2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,10 +2,11 @@
 """Create a TestAccessNestedMap class that
 inherits from unittest.TestCase"""
 from parameterized import parameterized
+from unittest.mock import patch, Mock
 import unittest
 
 
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -27,3 +28,20 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as e:
             access_nested_map(nested_map, path)
         self.assertEqual(f"KeyError('{expected_result}')", repr(e.exception))
+        
+class TestGetJson(unittest.TestCase):
+    """method to test that utils.get_json returns the expected result."""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    @patch('utils.requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+
+        result = get_json(test_url)
+
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(result, test_payload)
