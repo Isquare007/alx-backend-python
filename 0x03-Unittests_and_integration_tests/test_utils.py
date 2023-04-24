0a2bf0dd16b754602fec78dc0ab3c948f55aaa15
Isquare007 <akibrahiim2019@gmail.com>
2023-04-24 09:20:04 +0100

    remaining tasks

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,17 +19,18 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map(self, nested_map, path, expected_result):
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
-        
+
     @parameterized.expand([
         ({}, ("a",), "a"),
-        ({"a": 1,}, ("a", "b"), "b")
+        ({"a": 1, }, ("a", "b"), "b")
     ])
     def test_access_nested_map_exception(self, nested_map, path, expected_result):
         """ Test that a KeyError is raised for the respective inputs """
         with self.assertRaises(KeyError) as e:
             access_nested_map(nested_map, path)
         self.assertEqual(f"KeyError('{expected_result}')", repr(e.exception))
-        
+
+
 class TestGetJson(unittest.TestCase):
     """method to test that utils.get_json returns the expected result."""
     @parameterized.expand([
@@ -46,22 +47,29 @@ class TestGetJson(unittest.TestCase):
 
         mock_get.assert_called_once_with(test_url)
         self.assertEqual(result, test_payload)
-        
-class TestClass:
-    def a_method(self):
-        return 42
 
-    @memoize
-    def a_property(self):
-        return self.a_method()
 
-class TestMemoize(TestCase):
+class TestMemoize(unittest.TestCase):
+    """ Class for Testing Memoize """
+
     def test_memoize(self):
-        with mock.patch.object(TestClass, 'a_method') as mock_method:
-            instance = TestClass()
-            mock_method.return_value = 42
-            result1 = instance.a_property
-            result2 = instance.a_property
-            mock_method.assert_called_once()
-            self.assertEqual(result1, 42)
-            self.assertEqual(result2, 42)
+        """ Test that when calling a_property twice, the correct result
+        is returned but a_method is only called once using
+        assert_called_once
+        """
+
+        class TestClass:
+            """ Test Class for wrapping with memoize """
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mock:
+            test_class = TestClass()
+            test_class.a_property()
+            test_class.a_property()
+            mock.assert_called_once()
